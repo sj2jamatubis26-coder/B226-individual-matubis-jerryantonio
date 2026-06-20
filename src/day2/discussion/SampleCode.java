946ca708e3ef23e1a077583aa8ebde45ac9d30f8
jerry <mhaielysium@gmail.com>
2026-06-20 15:32:04 +0800

    Add discussion code for day2 and day3

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,20 @@
+
+package day3.discussion;
+
+
+public class SampleCode { // class
+    public static void main(String[] args) { // main method
+        System.out.println("Hello World"); //print statement
+    }
+}
+
+
+// Main Method -> starting point of programs execution
+// Print statement (sout) -> it tells the computer to print something
+/*
+    2 Types of Comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
+
+  
