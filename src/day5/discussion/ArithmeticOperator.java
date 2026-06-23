cb7726796575a56cea96404c8cfde0a6550a2d4c
jerry <mhaielysium@gmail.com>
2026-06-23 15:15:07 +0800

    operators day5

---
+++ src/day5/discussion/ArithmeticOperator.java
@@ -0,0 +1,20 @@
+
+package day5.discussion;
+
+
+public class ArithmeticOperator {
+    
+    public static void main(String[] args) {
+        int num = 50;
+        int num2 = 10;
+        int sum = num + num2;
+        
+        
+        System.out.println("sum: " + sum);
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num / num2);
+        
+    }
+    
+}
---
+++ src/day5/discussion/unaryOperator.java
@@ -0,0 +1,27 @@
+
+package day5.discussion;
+
+
+public class unaryOperator {
+    
+    public static void main(String[] args) {
+        // negating an expression (-)
+        int num = 10;
+        
+        System.out.println(num);
+        
+        // inverting the value of boolean (!)
+        boolean isEnrolled = false;
+        
+        System.out.println(!isEnrolled);
+        
+        // increment/decrement a value of one (++/--)
+        // pre-increment/post-increment
+        int number = 50;
+        
+        System.out.println(number++);
+        System.out.println(number);
+        //System.out.println(number--);
+    }
+    
+}
