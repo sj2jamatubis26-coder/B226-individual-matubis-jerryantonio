3a287ef1e14eb6c614b2e10f8e21f4b9818bf18a
jerry <mhaielysium@gmail.com>
2026-06-24 16:18:58 +0800

    Add discussion Day6

--- src/day6/discussion/logical_Operator.java
+++ src/day6/discussion/logical_Operator.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package day6.discussion;
 
-/**
- *
- * @author Admin
- */
+
 public class logical_Operator {
+    public static void main(String[] args) {
+  
+        // Cinema Logic
+        
+        boolean hasTicket = true;
+        boolean hasVaccinationCard = true;
+        boolean isSeniorCitizen = true;
+        boolean isStudent = true;
+        boolean isLoggedIn = false;
+        
+        // May ticket -> pwede pumasok sa sinehan
+        System.out.println("Allowed to enter cinema " + (hasTicket && hasVaccinationCard) );
+        
+        // May ticket (student || senior citizen) -> discounted
+        System.out.println("Eligible for Discount: " + (isStudent || isSeniorCitizen) );
+        
+        // Hindi naka logged in sa app
+        System.out.println("Cannot reserve seat: " + !isLoggedIn);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        int num = 100;
+//        int num2 = 200;
+//        
+//        boolean result = num < num2 && num > num2;
+//        System.out.println(result);
+        
+        /*  &&          Result
+            T   T       T
+            T   F       F
+            F   T       F
+            F   F       F
+        
+           /* ||           Result
+            T   T       T
+            T   F       T
+            F   T       T
+            F   F       F
+        */
+    }
     
 }
