447f547603f9c8afabb8eb9dd0cc00203df7787d
jerry <mhaielysium@gmail.com>
2026-06-24 15:13:01 +0800

    discussion for logical and relationa operator for day6

---
+++ src/day6/discussion/relational_Operator.java
@@ -0,0 +1,26 @@
+
+package day6.discussion;
+
+
+public class relational_Operator {
+    public static void main(String[] args) {
+        // < > <= >= == 
+        
+        int num = 100;
+        int num2 = 200;
+        
+        // boolean result = num < num2;
+        
+        System.out.println(num < num2);
+        System.out.println(num > num2);
+        System.out.println(num <= num2);
+        System.out.println(num >= num2);
+        System.out.println(num == num2);
+        System.out.println(num != num2);
+        
+        
+        
+        
+    }
+    
+}
